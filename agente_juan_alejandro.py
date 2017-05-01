8456b4272e4b99f018c1184f6aac990b74c11d57
Alejandro <alejo8a_2@hotmail.com>
2017-05-01 17:12:42 -0500

    algo

--- agente_juan_alejandro.py
+++ agente_juan_alejandro.py
@@ -1,3 +1,10 @@
+colores = {
+  "verde" : "1",
+  "amarillo" : "2",
+  "anaranjado" : "3",
+  "rojo" : "4"
+}
+
 def traducir_posicion(i, j):
   return i*5+j+1
 
@@ -17,8 +24,18 @@ class AgenteJ_A(object):
     self.oponentBoard = [[1/25 for x in range(5)] for y in range(5)]
     self.lastAction = None
 
-  def jugar(self):
-    pass
+
+  def jugar(self, resultado_accion, accion_oponente, estrellita):
+    if type(resultado_accion) is int:
+      print("Es un entero")
+    elif type(resultado_accion) is str:
+      print("es un string")
+    elif resultado_accion is None:
+      print("Es None")
+    print("Por aca")
+
+  def jugar_sensor(self, sensor):
 
 if __name__ == '__main__':
-  pass
+  a = AgenteJ_A(1)
+  a.jugar(None,0,0)
